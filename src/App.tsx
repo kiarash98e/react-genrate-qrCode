e0911860e0e85e126927c5aeb82f02b9d08fdcb1
kiarash98e <kiarashsafaei0@gmail.com>
2022-09-09 06:01:35 +0430

    genrate QrCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react'
+import InputColor from './components/inputColor/inputColor'
+import InputForm from './components/inputForm/inputForm'
+import QrCode from 'react-qr-code'
 
-function App() {
+const App:React.FC = () => {
+
+  const [inputVal,setInputVal] = React.useState({
+    url:'',
+    color:""
+  })
+  const [respone,setRes] = React.useState<any>()
+
+  const handleChange = (e:React.ChangeEvent) => {
+    const { value,name } = e.target as any
+    setInputVal({
+      ...inputVal,
+      [name]:value
+    })
+  }
+
+  const handleColor = (val:any) => {
+    setInputVal({
+      ...inputVal,
+      color:val
+    })
+  }
+
+  const handleQr = () => {
+    inputVal.url && inputVal.color && setRes(true)
+  }
+
+   
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-emerald-500 to-lime-700 h-screen pt-24 px-2">
+        <div className="container mx-auto rounded-md shadow-current max-w-4xl bg-white">
+            <div className="row-span-full flex justify-between gap-2">
+              <div className="col-span-full md:col-span-8 px-3 py-5">
+                  <InputForm name={"url"} value={inputVal.url} onChange={handleChange} />
+                  <InputColor name={"color"} value={inputVal.color} onChange={handleColor} />
+                  <button 
+                    onClick={handleQr}
+                    disabled={!inputVal.url}
+                    className='bg-lime-700 text-white ml-auto rounded-md max-w-xs py-2 px-4 
+                    mt-2 hover:bg-lime-600 disabled:bg-gray-400'>
+                      Genrate QrCode
+                  </button>
+              </div>
+              <div className="col-span-12 md:col-span-4 px-3 py-5">
+                  <div className="flex flex-col items-center bg-gray-100 rounded-r-md w-40 h-32 justify-center">
+                      
+                      {
+                        respone ? (
+                          <div>
+                            <QrCode
+                                title="qrcode"
+                                value={inputVal.url}
+                                fgColor={inputVal.color}
+                                className="w-auto h-auto"
+                                size={110}
+                              />
+                        </div>
+
+                        ): null
+                      }
+                  </div>
+              </div>
+            </div>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
